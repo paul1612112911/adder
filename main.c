0215431ea528d5c4ecee29fabd4f892b87f31670
張仁維 <harrison_chang@Harrisons-MacBook-Pro-2962.local>
2025-12-05 10:31:26 +0800

    feat: implement subtraction operation

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(){
-    printf("system checks OK\n");
+int main() {
+    printf("=== C Language Calculator ===\n");
+    
+    // Role C
+    int result_sub = 50 - 30;
+    printf("Subtraction: 50 - 30 = %d\n", result_sub);
+    
+    printf("System Check OK\n");
     return 0;
 }
