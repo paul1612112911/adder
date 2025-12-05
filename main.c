8df6f11639b6d8f91b3278ac6468d8e7ba49d95e
paul1612112911 <paul1612112911@gmail.com>
2025-12-05 11:06:21 +0800

    update main

--- main.c
+++ main.c
@@ -11,6 +11,14 @@ int sub_number(int a, int b){
 int main(){
     printf("=== C Language Calculator ===\n");
 
+    printf("1: add, 2: sub =>");
+    int choose;
+    scanf("%d", &choose);
+    int a, b;
+    scanf("%d%d", &a, &b);
+    if(choose == 1) printf("%d", add_number(a, b));
+    if(choose == 2) printf("%d", sub_number(a, b));
+
     printf("system checks OK\n");
     return 0;
 }
