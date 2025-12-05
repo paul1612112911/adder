3e0f430e87352ffbdc5a00392dcbd36b3a089e52
paul1612112911 <paul1612112911@gmail.com>
2025-12-05 11:03:25 +0800

    update sub

--- main.c
+++ main.c
@@ -4,13 +4,13 @@ int add_number(int a, int b){
     return a + b;
 }
 
+int sub_number(int a, int b){
+    return a - b;
+}
+
 int main(){
     printf("=== C Language Calculator ===\n");
 
-    // Role C
-    int result_sub = 50 - 30;
-    printf("Subtraction: 50 - 30 = %d\n", result_sub);
-    
     printf("system checks OK\n");
     return 0;
 }
